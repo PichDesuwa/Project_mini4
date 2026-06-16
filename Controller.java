edad235d450168facf7a32872c652de3e618e610
PichDesuwa <pichbothun@gmail.com>
2026-06-16 14:35:05 +0700

    addad

---
+++ Controller.java
@@ -0,0 +1,47 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private RadioButton D1;
+
+    @FXML
+    private RadioButton D2;
+
+    @FXML
+    private RadioButton D3;
+
+    @FXML
+    private DatePicker birth2;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private CheckBox c1;
+
+    @FXML
+    private CheckBox c2;
+
+    @FXML
+    private CheckBox c3;
+
+    @FXML
+    private TextField name1;
+
+    @FXML
+    private Label result;
+
+    @FXML
+    void onbutton(ActionEvent event) {
+//
+    }
+
+}
